f9e5f6acf8f2ac410d81ba6a35f4b6cc7a30a6c0
Xu Qi <helloassemble@gmail.com>
2016-01-29 12:04:20 +0800

    improve comment view,add footer and some css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,11 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:archive_id])
     @comment = @article.comments.build(comment_params)
     if @comment.save
-      flash[:success] = "Comment blog success!"
+      flash[:success] = "评论成功!"
       redirect_to archive_path(@article)
     else
       @comments = @article.comments
+      flash.now[:warning] = "评论失败!"
       render 'archives/show'
     end
   end
